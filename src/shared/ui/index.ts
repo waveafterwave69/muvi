8bad1ee9f0f62168ddb9beaddb74744da7bcec84
Michael Shonov <waveafterwave3w2@gmail.com>
2026-07-28 18:38:02 +0300

    feat(Card): ui component

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,1 +1,6 @@
-export { Button } from './Button/Button';
+import { Size } from '../types/size'
+import { Variant } from '../types/variant'
+import { Button } from './Button/Button'
+import Card from './Card/Card'
+
+export { Card, Button, type Size, type Variant }
