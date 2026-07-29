3a7cd6fec81a9efa82dad8aa41c82cf5f02a8972
Michael Shonov <waveafterwave3w2@gmail.com>
2026-07-29 13:40:39 +0300

    feat(Tabs): ui component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,5 @@
-'use client'
-
-import { useState } from 'react'
-import { Tab } from '@/shared/types/tab'
-import { Tabs } from '@/shared'
-
-const mainTabs: Tab[] = [
-    { label: 'Все', id: 1 },
-    { label: 'Фильмы', id: 2 },
-    { label: 'Сериалы', id: 3 },
-]
-
-const subTabs: Tab[] = [
-    { label: 'Избранные', id: 1 },
-    { label: 'Просмотренные', id: 2 },
-]
-
 const Home = () => {
-    const [activeMainTab, setActiveMainTab] = useState<number>(1)
-    const [activeSubTab, setActiveSubTab] = useState<number>(2)
-
-    return (
-        <>
-            <Tabs
-                variant="primary"
-                tabs={mainTabs}
-                value={activeMainTab}
-                onChange={setActiveMainTab}
-                size="md"
-            />
-
-            <Tabs
-                variant="secondary"
-                tabs={subTabs}
-                value={activeSubTab}
-                onChange={setActiveSubTab}
-                size="sm"
-            />
-        </>
-    )
+    return <></>
 }
 
 export default Home
